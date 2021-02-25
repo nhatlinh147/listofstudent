ccc323d3e1c07d4b37a9bd546a294299243485eb
nhatlinh147 <nguyennhatlinh1711@gmail.com>
2021-02-25 21:28:01 +0700

    Create list of staff by object and array

--- Baitaparray-listofS.js
+++ Baitaparray-listofS.js
@@ -1,30 +1,33 @@
 // Object
-var staff_one = {
-	name: Nguyễn Văn A,
-	phone: 012345678
+var student_one = {
+	name: 'Nguyễn Văn A',
+	code: 012345678
 	};
 
-var staff_two = {
-	name: Nguyễn Văn B,
-	phone: 876543210
+var student_two = {
+	name: 'Nguyễn Văn B',
+	code: 876543210
 	};
 	
-var staff_three = {
-	name: Nguyễn Văn C,
-	phone: 123456789
+var student_three = {
+	name: 'Nguyễn Văn C',
+	code: 123456789
 	};
-var staff_four = {
-	name: Nguyễn Văn D
-	phone: 987654321
+var student_four = {
+	name: 'Nguyễn Văn D',
+	code: 987654321
 	};
 
-var listofstaff_O = [staff_one,staff_two,staff_three,staff_four];
-console.log(listofstaff_O[1].name);
-console.log(listofstaff_O[1].phone);
+var listofstudent_O = [student_one,student_two,student_three,student_four];
+
+// hiển thị name và code của student_one
+console.log(listofstudent_O[1].name);
+console.log(listofstudent_O[1].code);
+
 // Array
-var listofstaff_A = [
-	{name: Nguyễn Văn A, phone: 012345678 },
-	{name: Nguyễn Văn B, phone: 876543210},
-	{name: Nguyễn Văn C, phone: 123456789},
-	{name: Nguyễn Văn D, phone: 987654321}
+var listofstudent_A = [
+	{name: 'Nguyễn Văn A', code: 012345678 },
+	{name: 'Nguyễn Văn B', code: 876543210},
+	{name: 'Nguyễn Văn C', code: 123456789},
+	{name: 'Nguyễn Văn D', code: 987654321}
 	];
