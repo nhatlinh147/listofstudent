38b571500b67fa030a4b8ea551176a30afb1e454
nhatlinh147 <nguyennhatlinh1711@gmail.com>
2021-02-25 20:33:37 +0700

    Create list of staff by oject and array

---
+++ Baitaparray-listofS.js
@@ -0,0 +1,30 @@
+// Object
+var staff_one = {
+	name: Nguyễn Văn A,
+	phone: 012345678
+	};
+
+var staff_two = {
+	name: Nguyễn Văn B,
+	phone: 876543210
+	};
+	
+var staff_three = {
+	name: Nguyễn Văn C,
+	phone: 123456789
+	};
+var staff_four = {
+	name: Nguyễn Văn D
+	phone: 987654321
+	};
+
+var listofstaff_O = [staff_one,staff_two,staff_three,staff_four];
+console.log(listofstaff_O[1].name);
+console.log(listofstaff_O[1].phone);
+// Array
+var listofstaff_A = [
+	{name: Nguyễn Văn A, phone: 012345678 },
+	{name: Nguyễn Văn B, phone: 876543210},
+	{name: Nguyễn Văn C, phone: 123456789},
+	{name: Nguyễn Văn D, phone: 987654321}
+	];
